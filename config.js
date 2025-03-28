c0d8599be10d55b0f9349c882384586f8d6f5b82
Jupiter Bold <sanadababy5@gmail.com>
2025-03-29 00:28:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,20 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-
+global.email = "jadewale71@gmail.com";
+global.location = "Lagos, Nigeria";
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://astrofx0011:astro@cluster0.lmwnxdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.allowJids = process.env.ALLOW_JID || "null";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
+global.github = process.env.GITHUB || "https://github.com/Jupiterbold05/Platinum-V1";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaeW5Tw4yltQOYIO5E2D";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeW5Tw4yltQOYIO5E2D";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/gvdQSvi.jpeg";
+global.devs = "2348084644182";
+global.sudo = process.env.SUDO || "2349071978357";
+global.owner = process.env.OWNER_NUMBER || "2348084644182";
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "h0hT0J6Z#_NeJoCmYvBlI9XQmmM_QNEpmTven2Ztr9sE1LdOsJAs",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/gzrefm.jpg",
